b2dc2e48c0b1bcab225f0f269a16d4124c9a77c4
soo0220 <163283719+soo0220@users.noreply.github.com>
2024-03-21 17:15:38 +0900

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,11 @@
+def get_radius(prompt):
+    r=int(input(prompt))
+    return r
+
+def get_circle_area(radius):
+    area=3.14*radius*radius
+    return area
+
+radius=get_radius("넓이를 구하고자 하는 원의 반지름은?")
+
+print("반지름",radius,"인 원의 넓이=3.14*",radius,"*",radius,"=",3.14*radius*radius)
