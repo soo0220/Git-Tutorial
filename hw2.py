563ea1468e03a749807cc206a26957fe2b294290
soo0220 <163283719+soo0220@users.noreply.github.com>
2024-04-26 17:38:27 +0900

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -19,3 +19,9 @@ print("500원 동전의 개수:", coin_counts['500원'])
 print("100원 동전의 개수:", coin_counts['100원'])
 print("50원 동전의 개수:", coin_counts['50원'])
 print("10원 동전의 개수:", coin_counts['10원'])
+#n500=money//500, money=money %500 순서,반복 후 print('500원동전개수:',n500)반복
+#def get_integer(prompt):
+#res = int(input(prompt))
+#return res
+#money =get_integer('동전교환액?')
+#exchange(money)
