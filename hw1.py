4db0362538cc593c2b6afbc4a9ffcb00bbfe5720
soo0220 <163283719+soo0220@users.noreply.github.com>
2024-04-26 16:26:20 +0900

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -10,7 +10,7 @@ radius=get_radius("넓이를 구하고자 하는 원의 반지름은?")
 
 print("반지름",radius,"인 원의 넓이=3.14*",radius,"*",radius,"=",3.14*radius*radius)
 
-#area를 함수 밖에서 의미있게 활용하고 싶다면
+#4/26 추가. area를 함수 밖에서 의미있게 활용하고 싶다면
 
 def get_radius(prompt):
     r = int(input(prompt))
